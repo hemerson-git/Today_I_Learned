90bd81bac72e862354a8772733171c045fb9b2db
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-09-28 21:56:19 -0300

    ADD useLayoutEffect

--- all-hooks/src/pages/useLayoutEffect.tsx
+++ all-hooks/src/pages/useLayoutEffect.tsx
@@ -1,21 +1,33 @@
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { GoBack } from "../components/GoBack";
 
 export function UseLayoutEffectExample() {
   console.log("Component");
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     console.log("useEffect");
-  }, []);
+  }, [counter]);
 
   useLayoutEffect(() => {
     console.log("useLayoutEffect");
-  }, []);
+  }, [counter]);
+
+  useLayoutEffect(() => {
+    for (let i = 0; i < 10000; i++) {
+      console.log(i);
+    }
+  }, [counter]);
 
   return (
-    <div>
+    <div className="container">
       <GoBack />
-      <h1>Hello</h1>
+      <h1 className="mb-1">Hello</h1>
+
+      <h2 className="center mb-2">{counter}</h2>
+      <button onClick={() => setCounter((prevState) => prevState + 1)}>
+        +
+      </button>
     </div>
   );
 }
